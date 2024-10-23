1fd99799550f2f636f85de448d6f3757375f682a
Vibhansh Bhatia <106678489+Monochrome901@users.noreply.github.com>
2024-10-23 08:42:12 +0530

    Delete fusion_detection directory

--- fusion_detection/file_utils.py
+++
@@ -1,7 +0,0 @@
-import os
-def create_folders(folder_name):
-    for affix in ["train", "val", "test", "test-challenge"]:
-        if not os.path.exists(os.path.join(".", folder_name, affix)):
-            os.makedirs(os.path.join(".", folder_name, affix))
-            os.makedirs(os.path.join(".", folder_name, affix, "images"))
-            os.makedirs(os.path.join(".", folder_name, affix, "annotations"))
